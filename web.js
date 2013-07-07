56e2ef74f9e26d0b8e601db7482abbccac0cbfa1
Ubuntu <ubuntu@ip-172-31-1-25.us-west-2.compute.internal>
2013-07-07 20:52:30 +0000

    modified web.js using fs.readFileSync and buf.toString

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.html);
+    fs.readFile('index.html', function (err, data){
+      if(err) throw err;
+      console.log(data);
+    });
+    read=buf.toString()
+  response.send(read);
 });
 
 var port = process.env.PORT || 5000;
