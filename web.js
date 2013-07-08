2e3464b17c149afb3e42d2f8378a1d3cecacdb4e
Ubuntu <ubuntu@ip-172-31-1-25.us-west-2.compute.internal>
2013-07-08 03:41:42 +0000

    removed var on fs.read

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var buffer = require('buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var data = fs.readFile('index.html', function (err, data) {
+fs.readFile('index.html', function (err, data) {
 	if (err) throw err;
 	console.log(data);
 });
