0c3743a5fc08a11feb94b911df6d1ce73b19029b
Ubuntu <ubuntu@ip-172-31-1-25.us-west-2.compute.internal>
2013-07-08 04:56:35 +0000

    attempt to have readFileSync output string directly

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var buffer = require('buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var read = fs.readFileSync("index.html","utf-8"), buffer.toString("utf-8"));
+    var read = fs.readFileSync("index.html","utf-8");
+//, buffer.toString("utf-8"));
     response.send(read);
 });
 
