17738fd89aa38fddd959257b4ed82f91887ea2cc
Ubuntu <ubuntu@ip-172-31-40-174.us-west-2.compute.internal>
2013-07-14 18:53:58 +0000

    modified web.js to listen on 8080 and modified index.html to prettify for hw4p1

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 var buffer = require('buffer');
+var htmlfile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var read = fs.readFileSync("index.html","utf-8");
-//, buffer.toString("utf-8"));
+    var read = fs.readFileSync(htmlfile).toString();
     response.send(read);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
